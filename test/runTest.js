2277b7cf1c2a09a28f41fcb71c97e0ee28f40f4a
Tosox <57193602+Tosox@users.noreply.github.com>
2025-03-18 18:38:00 +0100

    fix: ESLint errors

--- test/runTest.js
+++ test/runTest.js
@@ -8,9 +8,7 @@ async function main() {
             extensionTestsPath: path.resolve(__dirname, './suite/index'),
             launchArgs: [
                 '--disable-gpu', // Avoids GPU-related crashes
-                '--disable-software-rasterizer', // Reduces the need for a display
-                '--no-sandbox', // Prevents sandboxing issues
-                '--headless' // Forces VS Code to run in headless mode
+                '--no-sandbox' // Prevents sandboxing issues
             ],
         });
     } catch (err) {
