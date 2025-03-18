d027424fa037edcb4ffaac91f8302ad63223d51f
Tosox <57193602+Tosox@users.noreply.github.com>
2025-03-18 18:48:01 +0100

    fix: ESLint errors

--- test/runTest.js
+++ test/runTest.js
@@ -2,13 +2,16 @@ const { runTests } = require('@vscode/test-electron');
 const path = require('path');
 
 async function main() {
+    process.env.ELECTRON_NO_ATTACH_CONSOLE = '1'; // Prevents Electron from expecting a display
+    process.env.NO_AT_BRIDGE = '1'; // Disables some accessibility features that require a UI
+    process.env.DISPLAY = ''; // Ensures no X server is required
+
     try {
         await runTests({
             extensionDevelopmentPath: path.resolve(__dirname, '../'),
             extensionTestsPath: path.resolve(__dirname, './suite/index'),
             launchArgs: [
-                '--disable-gpu', // Avoids GPU-related crashes
-                '--no-sandbox' // Prevents sandboxing issues
+                '--disable-extensions' // Disables VS Code extensions
             ],
         });
     } catch (err) {
