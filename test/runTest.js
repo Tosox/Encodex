ad4ad33e20b8b3225035ed985d09822472ff4eac
Tosox <57193602+Tosox@users.noreply.github.com>
2025-03-18 18:55:21 +0100

    fix: ESLint errors

--- test/runTest.js
+++ test/runTest.js
@@ -2,11 +2,6 @@ const { runTests } = require('@vscode/test-electron');
 const path = require('path');
 
 async function main() {
-    process.env.DBUS_SESSION_BUS_ADDRESS = '/dev/null'; // Prevents Electron from trying to connect to DBus
-    process.env.ELECTRON_NO_ATTACH_CONSOLE = '1'; // Prevents Electron from expecting a display
-    process.env.NO_AT_BRIDGE = '1'; // Disables some accessibility features that require a UI
-    process.env.DISPLAY = ''; // Ensures no X server is required
-
     try {
         await runTests({
             extensionDevelopmentPath: path.resolve(__dirname, '../'),
