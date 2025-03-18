79df0ed0287dbb9ff5b26f0836f4d41d90b45d78
Tosox <57193602+Tosox@users.noreply.github.com>
2025-03-18 18:32:13 +0100

    chore: Update eslint.config.cjs

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,20 +1,24 @@
 module.exports = [
-	{
-	  languageOptions: {
-		ecmaVersion: 2018,
-		sourceType: "module"
-	  },
-	  linterOptions: {
-		reportUnusedDisableDirectives: true
-	  },
-	  rules: {
-		"no-const-assign": "warn",
-		"no-this-before-super": "warn",
-		"no-undef": "warn",
-		"no-unreachable": "warn",
-		"no-unused-vars": "warn",
-		"constructor-super": "warn",
-		"valid-typeof": "warn"
-	  }
-	}
-  ];
+  {
+    languageOptions: {
+      ecmaVersion: 2018,
+      sourceType: "module"
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true
+    },
+    environment: {
+      node: true,
+      mocha: true
+    },
+    rules: {
+      "no-const-assign": "warn",
+      "no-this-before-super": "warn",
+      "no-undef": "off",
+      "no-unreachable": "warn",
+      "no-unused-vars": "warn",
+      "constructor-super": "warn",
+      "valid-typeof": "warn"
+    }
+  }
+];
