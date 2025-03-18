e51f9291a95636953d292d1ade421cfa29923b00
Tosox <57193602+Tosox@users.noreply.github.com>
2025-03-18 18:25:57 +0100

    chore: Update .eslintrc.json

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+export default [
+	{
+	  languageOptions: {
+		ecmaVersion: 2018,
+		sourceType: "module"
+	  },
+	  linterOptions: {
+		reportUnusedDisableDirectives: true
+	  },
+	  rules: {
+		"no-const-assign": "warn",
+		"no-this-before-super": "warn",
+		"no-undef": "warn",
+		"no-unreachable": "warn",
+		"no-unused-vars": "warn",
+		"constructor-super": "warn",
+		"valid-typeof": "warn"
+	  }
+	}
+];
